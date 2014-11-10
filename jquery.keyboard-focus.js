49ef0c3e8f0ac54a1b100cf3df9bcb635521b807
Jason Gill <petethechop@gmail.com>
2014-11-10 16:15:06 -0700

    Create jquery.keyboard-focus.js

---
+++ jquery.keyboard-focus.js
@@ -0,0 +1,21 @@
+(function($){
+    var keyFocusConf = {
+        keyFocusClass : 'keyboard-focus',
+        mouseFocusClass : 'mouse-focus'
+    };
+    $(document).ready(function(){
+        $('body').trackFocus();
+    });
+    $.fn.trackFocus = function () {
+        var that = $(this);
+        that
+            .bind('mousedown', function(e){
+                that.removeClass(keyFocusConf.keyFocusClass);
+                that.addClass(keyFocusConf.mouseFocusClass);
+            })
+            .bind('keydown', function(e){
+                that.removeClass(keyFocusConf.mouseFocusClass);
+                that.addClass(keyFocusConf.keyFocusClass);
+            });
+    };
+})(jQuery);
