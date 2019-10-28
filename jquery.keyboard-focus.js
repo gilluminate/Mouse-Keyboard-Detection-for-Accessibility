cd4f56d97433705d73e3cd9d0237abf28e69f448
Jason Gill <jason@degreed.com>
2019-10-28 15:16:29 -0600

    FIX: indexOf error

--- jquery.keyboard-focus.js
+++ jquery.keyboard-focus.js
@@ -13,6 +13,9 @@
                 e.target.nodeName === 'TEXTAREA' ||
                 e.target.nodeName === 'CONTENTEDITABLE';
             // Watch for Tab key or any of the arrow keys
+            if (!e.key) {
+                return;
+            }
             if (e.key === 'Tab' ||
                 (e.key.indexOf('Arrow') === 0 && !ignoreArrowKeys)) {
                 bodyEl.removeClass(mouseFocusClass);
--- src/jquery.keyboard-focus.ts
+++ src/jquery.keyboard-focus.ts
@@ -14,6 +14,9 @@
         e.target.nodeName === 'TEXTAREA' ||
         e.target.nodeName === 'CONTENTEDITABLE';
       // Watch for Tab key or any of the arrow keys
+      if (!e.key) {
+          return;
+      }
       if (
         e.key === 'Tab' ||
         (e.key.indexOf('Arrow') === 0 && !ignoreArrowKeys)
